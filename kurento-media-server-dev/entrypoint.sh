4cabe2c38e85b76a56e9960dfcb9263bfdd8b4f5
patxi <patxi.gortazar@gmail.com>
2015-12-21 12:03:34 +0100

    [kurento-media-server-dev] Add coredumped facilities. Install kms public modules.
    Change-Id: Ib69561c37e98d9577dfde19402dec7914977812c

--- kurento-media-server-dev/entrypoint.sh
+++ kurento-media-server-dev/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+gdbbt() {
+ tmp=$(tempfile);
+ echo thread apply all bt >"$tmp";
+ gdb -batch -nx -q -x "$tmp" -e "$1" -c "$2";
+ rm -f "$tmp";
+}
+
 if [ ! -z "$COTURN_PORT_3478_TCP_ADDR" ]; then
   if [ ! -z "$COTURN_PORT_3478_TCP_PORT" ]; then
     # Generate WebRtcEndpoint configuration
@@ -12,4 +19,12 @@ fi
 # Remove ipv6 local loop until ipv6 is supported
 cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
 
-exec /usr/bin/kurento-media-server "$@"
+/usr/bin/kurento-media-server "$@"
+
+# Remove ipv6 local loop until ipv6 is supported
+cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
+/usr/bin/kurento-media-server "$@" || result=$?
+if [ $result == 134 ];
+then
+  gdbbt /usr/bin/kurento-media-server core | cat
+fi
