fb36ea37aaef7e241cb76dde6273b7d39ea5ed52
patxi <patxi.gortazar@gmail.com>
2016-01-18 12:17:10 +0100

    [kurento-media-server-dev] Remove core dump utilities
    Change-Id: I7a0aa1ca8e6aa36b688edb2085426e9bf50e7914

--- kurento-media-server-dev/entrypoint.sh
+++ kurento-media-server-dev/entrypoint.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
 set -e
-
-gdbbt() {
- tmp=$(tempfile);
- echo thread apply all bt >"$tmp";
- gdb -batch -nx -q -x "$tmp" -e "$1" -c "$2";
- rm -f "$tmp";
-}
 
 if [ ! -z "$COTURN_PORT_3478_TCP_ADDR" ]; then
   if [ ! -z "$COTURN_PORT_3478_TCP_PORT" ]; then
@@ -18,12 +11,7 @@ fi
 
 # Remove ipv6 local loop until ipv6 is supported
 cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
-ulimit -c
+
 cat /proc/sys/kernel/core_pattern
-/usr/bin/kurento-media-server $@ || result=$?
-echo "Kurento media server exited with $result"
-# This is ABORT
-if [ $result == 134 ];
-then
-  gdbbt /usr/bin/kurento-media-server core | cat
-fi
+
+exec /usr/bin/kurento-media-server "$@"
