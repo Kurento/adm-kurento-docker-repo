9946ab948dda6ece26efca5415315e0af79bb3cf
patxi <patxi.gortazar@gmail.com>
2016-03-11 15:45:04 +0100

    [dnat] Listen for 192.168 ips
    Change-Id: I454401d65ecfa92d40b04591a3b211446073873d

--- selenium/node-chrome-dev-debug/entry_point_dnat.sh
+++ selenium/node-chrome-dev-debug/entry_point_dnat.sh
@@ -2,12 +2,12 @@
 
 sleep 5s
 
-ip addr show|grep 192.168.0
+ip addr show|grep 192.168
 while [ $? != 0 ];
 do
   sleep 5s
-  echo "Looking for a 192.168.0.* IP"
-  ip addr show|grep 192.168.0
+  echo "Looking for a 192.168 IP"
+  ip addr show|grep 192.168
 done
 
 exec /opt/bin/entry_point.sh
